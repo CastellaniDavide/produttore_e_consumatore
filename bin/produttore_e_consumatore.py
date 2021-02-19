deaf3de9caf53e72dca24bdb8f687dde04a3912e
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-02-19 08:28:54 +0100

    Created bin/produttore_e_consumatore.py

---
+++ bin/produttore_e_consumatore.py
@@ -0,0 +1,19 @@
+"""produttore_e_consumatore
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-2-19"
+
+class produttore_e_consumatore:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(produttore_e_consumatore.produttore_e_consumatore())
+	
+	def produttore_e_consumatore():
+		"""produttore_e_consumatore first funtion
+		"""
+		return "produttore_e_consumatore" #for the test (see test_produttore_e_consumatore.py)
+		
+if __name__ == "__main__":
+	produttore_e_consumatore()
